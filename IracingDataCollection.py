297172fba8a9649595d0ee2bba9d73a7ac9a45e0
Maxwell Probala <probala24@gmail.com>
2026-01-05 19:46:28 -0500

    Add files via upload

---
+++ IracingDataCollection.py
@@ -0,0 +1,25 @@
+from iracingdataapi.client import irDataClient
+use_pydantic=True
+
+USERNAME = "probala24@gmail.com"
+PASSWORD = "Iamthebest#420"
+
+idc = irDataClient(access_token=[1405487]
+
+driver_Name = "Max Probala"
+
+drivers_lookup = idc.lookup_drivers(search_terms=driver_Name)
+
+if drivers_lookup and drivers_lookup.data:
+    customer_id = drivers_lookup.data[0]['cust_id']
+    print(f"Found {driver_Name} with Customer ID: {customer_id}")
+
+    driver_stats = idc.driver_stats(cust_id=customer_id)
+
+    if driver_stats and driver_stats.data:
+        print(f"Career iRating: {driver_stats.data['career_stats']['irating']}")
+        print(f"Career License Class: {driver_stats.data['career_stats']['license_class']}")
+    else:
+        print("Could not retrieve driver stats.")   
+else:
+    print(f"Driver '{driver_Name}' not found or an error occurred.")
